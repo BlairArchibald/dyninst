714b5c5eb06e78b627e453f583393d3ff3be2bbd
Bill Williams <bill@cs.wisc.edu>
2016-09-07 13:25:12 -0500

    Added interface for line info find-with-hint. Fixed dwarfWalker bug where we doubled up on compensating for one-based indexing. dwarfWalker now uses/fills a Module's stringtable directly. StringTables now streamable for debugging. Inlines use string table entries for filenames.

--- symtabAPI/src/Object-elf.C
+++ symtabAPI/src/Object-elf.C
@@ -4492,8 +4492,6 @@ void Object::parseTypeInfo()
     if(!typeInfo) return;
     DwarfWalker walker(associated_symtab, *typeInfo);
     walker.parse();
-    freeList.push_back(walker.getFreeList());
-//    freeList = walker.getFreeList();
 #if defined(TIMED_PARSE)
     struct timeval endtime;
   gettimeofday(&endtime, NULL);
